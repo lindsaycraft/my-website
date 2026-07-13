1472020aa514b6afd0308b3626af5826bd9479e5
lindsaycraft <lindsay.craft@sjsu.edu>
2026-07-12 23:16:53 -0700

    JS Fix

--- exploreTips.js
+++ exploreTips.js
@@ -1,11 +1,11 @@
 /* JavaScript for Adventure Suggestions */
 const suggestions = [
-    "#1 Go to Grounded for a coffee! ☕",
-    "#2 Head to Wingen for wood-fired pizza! 🍕",
-    "#3 Hop on Instagram and find a local event! 📱",
-    "#4 Do a chocolate and wine pairing at a Livermore winery! 🍷",
-    "#5 Stroll First Street and window shop under the lights! ✨",
-    "#6 Golden hour never misses out here...<br><img src='images/goldenhour.jpg' alt='Golden hour over Tri-Valley hills' width='300'>"
+    "Go to Grounded for a coffee! ☕",
+    "Head to Wingen for a pizza and pastry! 🍕",
+    "Hop on Instagram and find a local event! 📱",
+    "Go for a tasting at JMC winery 🍷",
+    "Browse for books at Towne Center Books ✨",
+    "Order a martini at Simply Fondue"
 ];
 
 document.getElementById('suggestButton').addEventListener('click', () => {
