a393d37169ceec58e52ad718605c0c0bc28b7a00
lindsaycraft <lindsay.craft@sjsu.edu>
2026-07-15 14:52:09 -0700

    Data Serialzation

--- fetch.js
+++ fetch.js
@@ -1,1 +1,26 @@
-
+window.onload = function() {
+     fetch('wineries.json')
+ 
+ .then(function(response) {
+ 
+return response.json();
+ 
+ })
+ 
+ .then(function(json) {
+ 
+var tableCode = '<table><caption>Livermore Valley Wineries</caption><thead><tr><th>Winery Name</th><th>Location</th><th>Year Established</th><th>Signature Wine</th></tr></thead><tbody>';
+ 
+for (var i = 0; i < json.length; i++) {
+ 
+ tableCode += '<tr><td>' + json[i].name + '</td><td>' + json[i].location + '</td><td>' + json[i].yearEstablished + '</td><td>' + json[i].signatureWine + '</td></tr>';
+ 
+ }
+ 
+ tableCode += '</tbody><tfoot><tr><td colspan="4">Source: Livermore Valley Winegrowers Association</td></tr></tfoot></table>';
+ 
+ document.getElementById('wineries').innerHTML = tableCode;
+ 
+ })
+ 
+ }
